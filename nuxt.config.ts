822580a5e303df6784973c7aa13fda4344a719da
HeavenSwordz <laurentbourget5@gmail.com>
2024-09-15 16:08:04 -0400

    chore: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-
-  app: {
-    head: {
-      title: "Portfolio | Laurent Bourget",
-      htmlAttrs: {
-        lang: "en",
-      },
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        {
-          hid: "description",
-          name: "description",
-          content:
-            "Frontend Web developer currently working as a Frontend Web developer for Cirque du Soleil",
-        },
-        { name: "format-detection", content: "telephone=no" },
-      ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    },
-  },
-
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: false },
   modules: ["@nuxt/image"],
 });
